4e96cc23eeecff9a15d0467fd5fad412fb1f18a8
sunrise <hxf6020@126.com>
2020-10-11 21:01:01 +0800

    update week2 homework. Signed-off-by: sunrise <hxf6020@126.com>

---
+++ Week_02/code/两数之和.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+	vector<int> twoSum(vector<int>& nums, int target) {
+		unordered_map<int, int> map;
+		vector<int> result;
+		for (int i = 0; i < nums.size(); i++) {
+			if (map.find(target - nums[i]) != map.end()) {
+				result.push_back(i);
+				result.push_back(map[target - nums[i]]);
+				return result;
+			}
+			else {
+				map[nums[i]] = i;
+			}
+		}
+		return result;
+
+	}
+};
